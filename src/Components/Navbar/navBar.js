a78180687656a37f9ce573c9d27c710caaf155f2
kiran shetty <kirannarayanks@gmail.com>
2023-09-01 15:09:19 +0530

    over all static ui completed

---
+++ src/Components/Navbar/navBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './nav.css';
+function navBar() {
+  return (
+    <div className='nav'>
+        <img className='logo' src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="netflix_logo" />
+        <img className='avatar' src="https://cdn-icons-png.flaticon.com/128/10307/10307911.png" alt="avatar" />
+    </div>
+  )
+}
+
+export default navBar
---
+++ src/Components/RowPost/RowPost.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './RowPost.css'
+function RowPost() {
+  return (
+    <div className='row'>
+        <h2>Thriller</h2>
+        <div className="posters">
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+            <img className='poster' src="https://wallpaperaccess.com/full/2331310.jpg" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default RowPost
